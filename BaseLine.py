4bae6fa86da62267f76c654277d587ce80e8e8d0
BingDaZhou <1095227326@qq.com>
2024-03-19 14:16:22 +0800

    修改BaseLine 的val_freq

--- BaseLine.py
+++ BaseLine.py
@@ -103,13 +103,13 @@ def main(args):
     # 记录选的是哪几个客户端，因为客户端权重跟其样本数量有关
     select_idx = [_ for _ in range(args.client_num)]
     for node_id in select_idx:
+        stime = time.time()
+        
         is_poison = False if node_id in clean_client_idx else True
-
         now_node: Local_node2 = node_list[node_id]
         train_merge_loader, train_clean_loader, test_clean_loader, test_backdoor_loader = \
             init_node_data(node_id, train_dataset,
                            test_dataset, subset_idx_list, args)
-
         print('Node_{:3d} Data Prepared | train_merge {:<4d} train_clean {:<4d} test_clean {:<4d} test_backdoor {:<4d}'.format(
             node_id, len(train_merge_loader), len(train_clean_loader), len(test_clean_loader), len(test_backdoor_loader)))
 
@@ -123,18 +123,16 @@ def main(args):
                                          now_node.scheduler, now_node.criterion, now_node.epoch + 1, now_node.args)
 
             if is_poison:
-                desc = 'Node_{:<3d} Poison Epoch {:<4d} Loss is {:4.2f}'
+                desc = 'Node_{:<3d} Poison Epoch {:<4d} Loss is {:6.4f}'
             else:
-                desc = 'Node_{:<3d} Clean  Epoch {:<4d} Loss is {:4.2f}'
+                desc = 'Node_{:<3d} Clean  Epoch {:<4d} Loss is {:6.4f}'
             print(desc.format(node_id, now_epoch+1, loss))
 
-            if (now_epoch+1) % 5 == 0 or now_epoch+1 == args.epochs:
-                # acc = validate(indices, test_clean_loader, model,
-                #                now_node.prompter, now_node.criterion, now_node.args)
-                # asr = validate(indices, test_backdoor_loader, model,
-                #                now_node.prompter, now_node.criterion, now_node.args)
-                acc = now_epoch + 10
-                asr = now_epoch + 10
+            if (now_epoch+1) % args.val_freq == 0 or now_epoch+1 == args.epochs:
+                acc = validate(indices, test_clean_loader, model,
+                               now_node.prompter, now_node.criterion, now_node.args)
+                asr = validate(indices, test_backdoor_loader, model,
+                               now_node.prompter, now_node.criterion, now_node.args)
                 now_node.acc = acc
                 now_node.asr = asr
                 now_node.save_checkpoint()
@@ -150,9 +148,12 @@ def main(args):
 
                 print(desc.format(
                     node_id, now_epoch + 1, acc, asr))
-
+        
+        etime = time.time()
+        print('time cosrt ', etime - stime)
 if __name__ == '__main__':
     fuck_args = parse_option()
     fuck_args.round = 1
+    fuck_args.val_freq = 5
     main(fuck_args)
     pass
